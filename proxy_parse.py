a5b9c752ae485075694a7fd0ec00e611e7d0a798
RSokolov <roman-27@inbox.ru>
2025-01-19 22:35:44 +0300

    Новые изменения

--- proxy_parse.py
+++ proxy_parse.py
@@ -1,9 +1,31 @@
 import requests
 import random
 
+import os
+from dotenv import load_dotenv
+
+
 def random_proxy_ip(file):
     """ метод возвращающий рандомный IP """
-    with open('proxy_list.txt', 'r') as file:
+    with open(file, 'r') as file:
         ip = random.choice(file.read().split())
         return ip
 
+
+def random_user_agent(file):
+    """ метод возвращающий рандомный user-agent """
+    with open(file, 'r') as file:
+        ua = random.choice(file.read().split('\n'))
+        return ua
+    
+
+proxy_ip = random_proxy_ip('proxy_list.txt')
+
+
+proxies = {
+    'http': 'socks5://f"{proxy_ip}"',
+    'https': 'socks5://f"{proxy_ip}"'
+}
+
+print(random_proxy_ip('proxy_list.txt'))
+print(random_user_agent('user_agent.txt'))

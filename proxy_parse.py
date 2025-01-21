7201d3f80edd9dc7581fc16e2d8dd1c60c499af2
RSokolov <roman-27@inbox.ru>
2025-01-21 16:22:50 +0300

    Наворотил, ахренеть!

--- proxy_parse.py
+++ proxy_parse.py
@@ -1,8 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 import random
+import pprint
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
 
 def random_proxy_ip(file):
@@ -20,12 +22,40 @@ def random_user_agent(file):
     
 
 proxy_ip = random_proxy_ip('proxy_list.txt')
+headers = random_user_agent('user_agent.txt')
+
 
 
 proxies = {
-    'http': 'socks5://f"{proxy_ip}"',
-    'https': 'socks5://f"{proxy_ip}"'
+    'http': 'http://f"{proxy_ip}"',
+    # 'https': 'https://f"{proxy_ip}"'
 }
 
-print(random_proxy_ip('proxy_list.txt'))
-print(random_user_agent('user_agent.txt'))
+print(type(proxy_ip))
+print(type(headers))
+# # url='https://www.wildberries.ru/'
+
+# s = requests.Session()
+# response = requests.get(url='https://www.wildberries.ru/catalog/krasota/nogti/nakladnye-nogti-i-dekor?page=1',headers=user_agent, proxies=proxies)
+
+# print(response.status_code)
+# s = Session()
+# req = Request('GET',  url)
+
+# prepped = s.prepare_request(req)
+
+# print(req.headers) 
+
+
+
+def get_location(url):
+    response = requests.get(url,proxies='https://fwafaq:KjwouzQ1www@46.8.15.143:5500')
+    soup = BeautifulSoup(response.text, 'lxml')
+    
+    ip = soup.find('div', class_="js-value-copy cc-text cc-text--h3 cc-text--bold").text.split()
+   
+    print(ip)
+
+
+
+get_location('https://2domains.ru/web-tools/myip')

d37ff0a4f201c41ebf733c1d51bc69cdf10d0cd1
RSokolov <roman-27@inbox.ru>
2025-01-28 16:05:46 +0300

    add

--- parser_selenium.py
+++ parser_selenium.py
@@ -1,23 +1,51 @@
+import os
 import requests
 import pprint
 
-URL = "https://catalog.wb.ru/catalog/beauty44/v2/catalog?ab_testing=false&appType=1&cat=8956&curr=rub&dest=-1255987&hide_dtype=10&lang=ru&page=1&sort=popular&spp=30"
+from dotenv import load_dotenv, find_dotenv
+from requests_ip_rotator import ApiGateway
+from randomheader import RandomHeader
 
-HEADERS = {'access-control-allow-credentials':'true',
-'access-control-allow-headers':'Authorization, x-captcha-id',
-'access-control-allow-methods':'HEAD,GET,OPTIONS',
-'access-control-allow-origin':'https://www.wildberries.ru',
-'content-encoding':'gzip',
-'content-type':'application/json; charset=utf-8'
-}
+load_dotenv(find_dotenv())
+
+KEY = os.getenv('ACCESS_KEY')
+SECRET_KEY = os.getenv('SECRET_ACCESS_KEY')
+random_headers = RandomHeader()
 
 
-response = requests.get(URL, headers=HEADERS)
+
+random_headers = random_headers.header()
+
+BASE_URL = 'https://www.wildberries.ru/'
+
+URL = "https://catalog.wb.ru/catalog/beauty44/v2/catalog?ab_testing=false&appType=1&cat=8956&curr=rub&dest=-1255987&hide_dtype=10&lang=ru&sort=popular&spp=30"
+
+gateway = ApiGateway(BASE_URL, access_key_id=KEY, access_key_secret=SECRET_KEY )
+gateway.start()
+
+# Assign gateway to session
+session = requests.Session()
+session.mount(BASE_URL, gateway)
+
+# Send request (IP will be randomised)
+response = session.get(URL, params={"page": 0 }, headers=random_headers)
 print(response.status_code)
 response_json = response.json()
-
 total_pages = response_json['data']['total']
 print(total_pages)
-index_sellers = response_json['data']['products'][1]  #['supplierId']
-print(index_sellers['supplierId'])
-pprint.pprint((response_json['data']['products'][99]))
+
+
+# Delete gateways
+gateway.shutdown()
+
+
+
+# response = requests.get(URL, headers=random_headers)
+# print(response.status_code)
+# response_json = response.json()
+
+# total_pages = response_json['data']['total']
+# print(total_pages)
+# index_sellers = response_json['data']['products'][1]  #['supplierId']
+# print(index_sellers['supplierId'])
+# pprint.pprint((response_json['data']['products'][99]))
--- test.py
+++ test.py
@@ -1,66 +1,85 @@
-import ipaddress
-from random import choice, randint
 import os
+import requests
+import pprint
+
 from dotenv import load_dotenv, find_dotenv
-
-from httpx import Request, URL
-from requests_ip_rotator import ApiGateway, MAX_IPV4
-import httpx
-import asyncio
-
+from requests_ip_rotator import ApiGateway
+from randomheader import RandomHeader
 
 load_dotenv(find_dotenv())
 
 KEY = os.getenv('ACCESS_KEY')
 SECRET_KEY = os.getenv('SECRET_ACCESS_KEY')
 
-PARSING_URL = 'https://www.avito.ru/moskva?q='
-BASE_URL = 'https://www.avito.ru'
-HEADERS = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-language": "en-US,en;q=0.9,ru;q=0.8",
-    "cache-control": "max-age=0",
-    "if-none-match": "W/\"2fba1a-vj8ZZTJXtBDfFHvTGM6hmxDjuNE\"",
-    "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"macOS\"",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1"
-}
+random_headers = RandomHeader()
+random_headers = random_headers.header()
+
+BASE_URL = 'https://www.wildberries.ru/'
+
+URL = "https://catalog.wb.ru/catalog/beauty44/v2/catalog?ab_testing=false&appType=1&cat=8956&curr=rub&dest=-1255987&hide_dtype=10&lang=ru&sort=popular&spp=30"
+TOTAL_URL = "https://catalog.wb.ru/catalog/beauty44/v2/catalog?ab_testing=false&appType=1&cat=8956&curr=rub&dest=-1255987&hide_dtype=10&lang=ru&page=1&sort=popular&spp=30"
 
 
-class AWSGatewayTransport(ApiGateway, httpx.AsyncHTTPTransport):
-
-    async def handle_async_request(self, request: Request):
-        endpoint = choice(self.endpoints)
-        # Replace URL with our endpoint
-        request.url = URL("https://" + endpoint + "/ProxyStage/" + request.url.path)
-        # Replace host with endpoint host
-        request.headers["Host"] = endpoint
-        # Auto generate random X-Forwarded-For if doesn't exist.
-        # Otherwise AWS forwards true IP address in X-Forwarded-For header
-        x_forwarded_for = request.headers.get("X-Forwarded-For")
-        if x_forwarded_for is None:
-            x_forwarded_for = ipaddress.IPv4Address._string_from_ip_int(randint(0, MAX_IPV4))
-        # Move "X-Forwarded-For" to "X-My-X-Forwarded-For". This then gets converted back
-        # within the gateway.
-        request.headers.pop("X-Forwarded-For", None)
-        request.headers["X-My-X-Forwarded-For"] = x_forwarded_for
-        # Run original python requests send function
-        return await super().handle_async_request(request)
+gateway = ApiGateway(BASE_URL, access_key_id=KEY, access_key_secret=SECRET_KEY )
+gateway.start()
+session = requests.Session()
+# session.mount(BASE_URL, gateway)
 
 
-async def main():
-    with AWSGatewayTransport(BASE_URL, access_key_secret=KEY, access_key_id=SECRET_KEY) as g:
-        mounts = {
-            "all://": g,
-        }
-        async with httpx.AsyncClient(mounts=mounts) as client:
-            for index in range(5):
-                res = await client.get(PARSING_URL + str(index), headers=HEADERS)
-                print(res)
 
-asyncio.run(main())
+response = session.get(TOTAL_URL, headers=random_headers)
+response_json = response.json()
+total_pages = response_json['data']['total']
+print(total_pages)
+
+def parser_wb(URL):
+    pass
+
+gateway.shutdown()
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+gateway = ApiGateway(BASE_URL, access_key_id=KEY, access_key_secret=SECRET_KEY )
+gateway.start()
+
+# Assign gateway to session
+session = requests.Session()
+session.mount(BASE_URL, gateway)
+
+# Send request (IP will be randomised)
+response = session.get(URL, params={"page": 2 }, headers=random_headers)
+print(response.status_code)
+response_json = response.json()
+total_pages = response_json['data']['total']
+print(total_pages)
+
+
+# Delete gateways
+gateway.shutdown()
+
+
+
+# response = requests.get(URL, headers=random_headers)
+# print(response.status_code)
+# response_json = response.json()
+
+# total_pages = response_json['data']['total']
+# print(total_pages)
+# index_sellers = response_json['data']['products'][1]  #['supplierId']
+# print(index_sellers['supplierId'])
+# pprint.pprint((response_json['data']['products'][99]))
